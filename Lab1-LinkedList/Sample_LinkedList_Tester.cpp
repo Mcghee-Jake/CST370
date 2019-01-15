faeec413fcc5a643c5564fc48fc01d12839b8a93
Mcghee-Jake <jmcghee@csumb.edu>
2019-01-15 13:55:45 -0800

    started lab1

---
+++ Lab1-LinkedList/Sample_LinkedList_Tester.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+#include "LinkedList.h"
+
+int main()
+{
+   // Test the class constructor
+   LinkedList intList;
+   cout << "Constructing intList\n";
+
+   // Test insert()
+   intList.insert(100, 0);
+   intList.display(cout);
+   cout << endl;
+
+   intList.insert(200, 0);
+   intList.display(cout);
+   cout << endl;
+   
+   intList.insert(300, 1);
+   intList.display(cout);
+   cout << endl;
+   
+   intList.insert(400, 1);
+   intList.display(cout);
+   cout << endl;
+   
+   intList.insert(500, 3);
+   intList.display(cout);
+   cout << endl;
+
+   // Test destructor
+   LinkedList anotherList;
+   for (int i = 0; i < 10; i++)
+   {
+	   anotherList.insert(100*i, i);
+   }
+   cout << "\nThis is another list\n";
+   anotherList.display(cout);
+  
+
+   // Test erase
+   intList.erase(1);
+   intList.erase(1);
+   cout << "\n\nTwo items are erased from the first list\n";
+   intList.display(cout);
+   cout << endl << endl;
+
+   // Test maxItem();
+   cout << "Max Item (first list): " << intList.maxItem(cout) << endl;
+   cout << "Max Item (second list): " << anotherList.maxItem(cout) << endl;
+   LinkedList singleItemList;
+   singleItemList.insert(100, 0);
+   cout << "New list with a single item: ";
+   singleItemList.display(cout);
+   cout << "\nMax Item: " << singleItemList.maxItem(cout) << endl;
+   LinkedList emptyList;
+   cout << "New empty list: ";
+   emptyList.display(cout);
+   cout << endl;
+   cout << "Max Item: " << emptyList.maxItem(cout) << endl;
+
+   cout << endl;
+
+}

0d42cfa3a9a3a039ded9d326d36866b25f04f4c8
Mcghee-Jake <jmcghee@csumb.edu>
2019-01-16 14:17:51 -0800

    completed lab 2 part b

--- Lab2-Stacks/Sample_Stack_tester.cpp
+++ Lab2-Stacks/Sample_Stack_tester.cpp
@@ -10,19 +10,30 @@ using namespace std;
 
 int main()
 {
-   Stack s;
-   
 
-   cout << "Enter a string => ";
-   string input;
-   cin >> input;
-   for (int i = 0; i < input.length(); i++)
-      s.push(input[i]);
-   cout << "You entered " << input << "\nReverse is ";
-   while (!s.empty())
-   {
-	   cout << (char)s.top();
-	   s.pop();
-   }
-   cout << endl;
+	Stack s1;
+	cout << "Enter a string => ";
+	string input;
+	cin >> input;
+	for (int i = 0; i < input.length(); i++)
+		s1.push(input[i]);
+	cout << "You entered " << input << "\nReverse is ";
+	while (!s1.empty())
+	{
+		cout << (char)s1.top();
+		s1.pop();
+	}
+	cout << endl;
+
+	Stack s2;
+	cout << "Enter a number: ";
+	int num;
+	cin >> num;
+	while (num > 0) {
+		s2.push(num % 2);
+		num /= 2;
+	}
+	s2.display(cout);
+
+
 }
